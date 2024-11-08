96ffebb709448760a8a4b3c3e45ce5a251a9b1cb
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2024-11-08 11:25:42 +0530

    Time: 0 ms (100.00%), Space: 49.2 MB (10.01%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+  
+    
+    return n%2==0 ? n : 2*n
+    
+    
+    
+    
+};
+
+smallestEvenMultiple(5)

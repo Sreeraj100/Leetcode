8e19ec16f5eee1e79e59e0f80aeafd30f8086bdf
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2025-01-12 20:18:15 +0530

    Time: 0 ms (100%), Space: 48.4 MB (98.65%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+    
+let a=BigInt(num1)
+let b=BigInt(num2)
+let out=a*b
+return out.toString()
+
+
+};

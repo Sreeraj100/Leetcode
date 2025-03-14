34823d52606914cdef5ebbbf967c9648f47efe62
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2025-03-14 09:57:37 +0530

    Time: 0 ms (100%), Space: 54.8 MB (38.34%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -5,14 +5,14 @@
  */
 var countPairs = function(nums, target) {
     let count=0
-    for(let j=0;j<nums.length;j++){
-    for(let i=1;i<nums.length;i++){
-        if(j<i){
-        if(nums[j]+nums[i]<target){
-          count++
-        }
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+         if(nums[i]+nums[j]<target){
+            count++
+         }
+
         }
     }
- }
-   return count 
+    return count
+
 };

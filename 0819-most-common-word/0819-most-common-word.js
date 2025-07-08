570374e0da6a611aefc56d8b5c5f9c865ab96f31
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2025-07-08 23:55:09 +0530

    Time: 4 ms (63.78%), Space: 57.8 MB (16.76%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.js
+++ 0819-most-common-word/0819-most-common-word.js
@@ -4,25 +4,25 @@
  * @return {string}
  */
 var mostCommonWord = function(paragraph, banned) {
-   paragraph = paragraph.toLowerCase().replace(/[^a-z\s]/g, ' ');
+   paragraph = paragraph.toLowerCase().replace(/[^a-z\s]/g, ' ')
     
-    let arr = paragraph.split(/\s+/); // split by one or more spaces
-    let obj = {};
+    let arr = paragraph.split(/\s+/)
+    let obj = {}
 
     for (let i of arr) {
-        if (i === '' || banned.includes(i)) continue; // skip empty or banned words
-        obj[i] ? obj[i]++ : obj[i] = 1;
+        if (i === '' || banned.includes(i)) continue
+        obj[i] ? obj[i]++ : obj[i] = 1
     }
 
     let lar = 0;
-    let word = '';
+    let word = ''
     for (let i in obj) {
         if (obj[i] > lar) {
-            lar = obj[i];
-            word = i;
+            lar = obj[i]
+            word = i
         }
     }
 
-    return word;
+    return word
 
 };

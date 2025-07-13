56401e6c48ae219437b9bc88508bb06bed8603b5
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2025-07-13 23:57:28 +0530

    Time: 16 ms (51.36%), Space: 57.9 MB (55.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -5,15 +5,25 @@
  */
 var isAnagram = function(s, t) {
     
-    if(s.length !== t.length) return false
-    let count={}
-    for(let i of s){
-        count[i]?count[i]++:count[i]=1
-    }
-    for(let i of t){
-        if(!count[i]) return false
-        count[i]--
+  if(s.length!=t.length)return false;
+    let map = new Map();
+    for(let i=0;i<s.length;i++){
+        map.set(s[i],(map.get(s[i])||0)+1);
     }
 
-    return true
+    for(let i=0;i<t.length;i++){
+        if(map.has(t[i]) && map.get(t[i])>0){
+            map.set(t[i],map.get(t[i])-1);
+        }else{
+            return false;
+        }
+    }
+
+    for(let val of map.values()){
+        if(val!=0){
+            return false;
+        }
+    }
+
+    return true;
 };

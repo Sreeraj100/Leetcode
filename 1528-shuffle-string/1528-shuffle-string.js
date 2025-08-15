a60cfcd4fa7eed5d1f78c55d95363ec3cccb5e92
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2025-08-15 23:59:35 +0530

    Time: 2 ms (48.25%), Space: 55.6 MB (73.19%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.js
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -4,9 +4,13 @@
  * @return {string}
  */
 var restoreString = function(s, indices) {
-    const result = [];
-    for(let i=0;i<s.length;i++) {
-        result[indices[i]]=s[i]
+    let arr = [];
+    let result = "";
+    for(let i=0; i<indices.length; i++) {
+        arr[indices[i]] = s.charAt(i);
     }
-    return result.join('');
+    for(let i=0; i<arr.length; i++) {
+        result = result + arr[i];
+    }
+    return result;
 };

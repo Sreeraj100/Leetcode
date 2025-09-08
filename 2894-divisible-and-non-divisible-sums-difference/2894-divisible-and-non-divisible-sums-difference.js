d271d21ab9e7816771fa09a902fcbdf44e3abbf6
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2025-09-08 23:52:33 +0530

    Time: 1 ms (39.35%), Space: 56 MB (15.21%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -4,15 +4,16 @@
  * @return {number}
  */
 var differenceOfSums = function(n, m) {
-      let num1 = 0, num2 = 0;
+let nonDivSum = 0;
+    let divSum = 0;
 
     for (let i = 1; i <= n; i++) {
-        if (i % m === 0) {
-            num2 += i;
+        if (i % m == 0) {
+            divSum += i;
         } else {
-            num1 += i;
+            nonDivSum += i;
         }
     }
 
-    return num1 - num2;  
+    return nonDivSum - divSum;
 };

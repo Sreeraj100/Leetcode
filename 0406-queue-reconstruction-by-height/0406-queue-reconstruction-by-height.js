146d7d9b6f5ea2e6ff341336c4ae9ff4fff37863
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2025-09-15 06:13:35 +0530

    Time: 5 ms (75.56%), Space: 63.6 MB (15.56%) - LeetHub

---
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[][]} people
+ * @return {number[][]}
+ */
+var reconstructQueue = function(people) {
+ people.sort((a, b) => b[0] - a[0] || a[1] - b[1]);
+    const res = [];
+    for (let p of people) {
+        res.splice(p[1], 0, p);
+    }
+    return res;
+};

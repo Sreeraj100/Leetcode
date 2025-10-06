0bc80be86d547d77918c9c718b3003b578e2ebd0
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2025-10-07 00:00:04 +0530

    Time: 33 ms (33.02%), Space: 54.6 MB (71.89%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-       let arr=[]
-for (let i=0;i<nums.length;i++){
-  for(let j=i+1;j<nums.length;j++){  
-    if(nums[i]+nums[j]==target){
-        
-    arr.push(i,j)
-    
-  }    
-}
-}
-return arr
+     for(var i=0;i<nums.length;i++){
+        for(var j = i+1;j<nums.length;j++){
+            if(nums[i]+nums[j] == target){
+                return [i,j]
+            }
+        }
+    }
 };

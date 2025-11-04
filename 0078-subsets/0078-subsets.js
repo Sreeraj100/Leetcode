1fa603c1b325e0501475535479bb85700be77a39
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2025-11-04 23:57:11 +0530

    Time: 0 ms (100%), Space: 57.8 MB (37.25%) - LeetHub

---
+++ 0078-subsets/0078-subsets.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+       let res = [[]], appendarr= []
+    
+    for(let num of nums){
+        appendarr = []
+        for(let entry of res){
+            appendarr.push([...entry, num])
+        }
+        
+        res.push(...appendarr)
+    }
+    
+    return res 
+};

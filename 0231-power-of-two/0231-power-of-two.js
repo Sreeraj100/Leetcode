7d39a309b873f51b32ece1f62c4138605acd0f29
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2025-12-03 14:39:05 +0530

    Time: 0 ms (100%), Space: 56.2 MB (33.7%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -3,6 +3,14 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-   if (n <= 0) return false;
-    return (n & (n - 1)) === 0;
+    if(n<1){
+        return false
+    }
+    while(n>1){
+        if(n%2 !== 0){
+            return false
+        }
+        n = n/2
+    }
+    return true
 };

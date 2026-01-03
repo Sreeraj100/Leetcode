6553527bbd4085b2aae44f6a57e0bab739e0b0a6
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2026-01-04 00:04:00 +0530

    Time: 3 ms (77.32%), Space: 56 MB (67.95%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function(s, k) {
+ if (k > s.length) return s.split('').reverse().join('')
+    
+    const split = s.split('')
+    
+    for (let i = 0; i < s.length; i += 2*k) {
+        const reverse = split.splice(i, k).reverse()
+        split.splice(i, 0, ...reverse)
+    }
+    
+    return split.join('')
+}

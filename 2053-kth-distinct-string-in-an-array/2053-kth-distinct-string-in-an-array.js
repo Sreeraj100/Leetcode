45ef940883931808a1747d24255065390f030889
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2026-01-17 23:59:53 +0530

    Time: 6 ms (60%), Space: 57.6 MB (57.27%) - LeetHub

---
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} arr
+ * @param {number} k
+ * @return {string}
+ */
+var kthDistinct = function(arr, k) {
+  let obj = {};
+    let count = 0;
+    
+    for(let i = 0; i < arr.length; i++){
+            obj[arr[i]] = obj[arr[i]] + 1 || 1
+    }
+    
+
+    for(let j = 0; j < arr.length; j++){
+        if(obj[arr[j]] == 1 ){
+            count++
+            if(count == k){
+                return arr[j]
+            }  
+        } 
+    }
+    
+    if(count != k){
+        return ""
+    }  
+};

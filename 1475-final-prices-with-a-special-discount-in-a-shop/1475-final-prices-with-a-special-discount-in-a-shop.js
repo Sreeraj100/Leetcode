8cfd7d14680527739d91cc005788ceefff6dfbcb
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2026-01-27 17:56:53 +0530

    Time: 1 ms (64.87%), Space: 58.2 MB (26.79%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number[]}
+ */
+var finalPrices = function(prices) {
+    let ans=[]
+    for(let i=0;i<prices.length;i++){
+        let min=0
+        for(let j=i+1;j<prices.length;j++){
+            if(prices[j] <= prices[i]){
+                min=prices[j]
+                break
+            }
+        }
+        ans.push(prices[i]-min)
+    }
+    return ans
+};

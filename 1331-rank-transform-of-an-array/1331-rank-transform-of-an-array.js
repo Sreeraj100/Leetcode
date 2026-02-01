9e83e7f994dde77c1a2130e718e145eacb465466
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2026-02-01 19:11:02 +0530

    Time: 98 ms (23.7%), Space: 86.3 MB (22.22%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.js
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.js
@@ -3,16 +3,18 @@
  * @return {number[]}
  */
 var arrayRankTransform = function(arr) {
-    
 let Rank=[...new Set(arr)]
 Rank.sort((a,b)=>a-b)
 let res=[]
-console.log(Rank)
+let obj={}
+for(let i=0;i<Rank.length;i++){
+    if(!obj[Rank[i]]){
+        obj[Rank[i]]=i+1
+    }
+}
 for(let i=0;i<arr.length;i++){
-    res.push(Rank.indexOf(arr[i])+1)
+    res.push(obj[arr[i]])
     
 }
 return res
-
-
 };

e03da906e219212700e36678896ecf8a2108c6f3
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2026-03-21 19:05:36 +0530

    Time: 2 ms (61.12%), Space: 54.9 MB (97.43%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+        let count=n
+        for(let i =0; i<flowerbed.length; i++){
+        let curr= flowerbed[i]
+        if(curr==1) continue
+        if(curr==0){
+            let left = (flowerbed[i-1]==0) || (i==0)
+            let right = (flowerbed[i+1]==0) || (i==flowerbed.length-1)
+            if(left && right){
+                count--
+                flowerbed[i]=1
+            }
+            if(count<=0) return true
+        }
+    }
+    return count<=0
+
+};

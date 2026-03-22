b33125a81b4b3193a1ae0c81d8ffbe36191c017f
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2026-03-22 23:59:41 +0530

    Time: 5 ms (76.36%), Space: 76.3 MB (72.73%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var deepestLeavesSum = function(root) {
+     if (!root)
+        return 0
+    
+    let cache = []
+    const rec = (root, depth = 0) => {
+        if (!root)
+            return 0
+        
+        
+        cache[depth] = (cache[depth] || 0) + root.val
+        
+        rec(root.left, depth + 1)
+        rec(root.right, depth + 1)
+    }
+    
+    rec(root)
+    return cache[cache.length - 1]
+};

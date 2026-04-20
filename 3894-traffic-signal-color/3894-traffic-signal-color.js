143028d594e1780f0942afb840dbd551792342a8
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2026-04-21 00:54:26 +0530

    Time: 0 ms (100%), Space: 56.6 MB (11.71%) - LeetHub

---
+++ 3894-traffic-signal-color/3894-traffic-signal-color.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} timer
+ * @return {string}
+ */
+var trafficSignal = function(timer) {
+    if (timer === 0) {
+        return "Green"
+    } else if (timer === 30) {
+        return "Orange"
+    } else if (30 < timer && timer <= 90) {
+        return "Red"
+    }
+    return "Invalid"
+};

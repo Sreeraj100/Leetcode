ed8e69b072c9ef00069abfcd88a90ea5d4be45ba
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2026-05-11 00:00:06 +0530

    Time: 2 ms (60.82%), Space: 57.2 MB (78.9%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.js
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.js
@@ -3,17 +3,16 @@
  * @return {number[]}
  */
 var countOppositeParity = function(nums) {
-    const result = []
-
-  for (let i = 0; i < nums.length; i++) {
-    let count = 0
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] % 2 !== nums[j] % 2) {
-        count++
-      }
+   let odd = 0, even = 0
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] % 2 == 0) {
+            even++
+            nums[i] = odd
+        }
+        else {
+            odd++
+            nums[i] = even
+        }
     }
-    result.push(count)
-  }
-
-  return result
-};
+    return nums
+}

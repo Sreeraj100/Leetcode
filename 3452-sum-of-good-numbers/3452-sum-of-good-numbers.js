34cdbaa0452d7c7cd912f8159d629c50f0cf0e82
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2026-06-10 02:02:05 +0530

    Time: 2 ms (34.04%), Space: 56 MB (46.81%) - LeetHub

--- 3452-sum-of-good-numbers/3452-sum-of-good-numbers.js
+++ 3452-sum-of-good-numbers/3452-sum-of-good-numbers.js
@@ -5,12 +5,10 @@
  */
 var sumOfGoodNumbers = function(nums, k) {
     let sum=0
-    for(let i=0;i<nums.length;i++){
-        let n=nums[i+k]?? 0
-        let n2=nums[i-k]?? 0
-        if(nums[i]>n && nums[i]>n2){
-            sum+=nums[i]
-        }
+    for (i=0; i<nums.length; i++) {
+        if ((typeof nums[i+k]==='undefined'||nums[i+k]<nums[i])&&(typeof nums[i-k]==='undefined'||nums[i-k]<nums[i]) ) {            
+         sum = sum + nums[i];            
+        }   
     }
     return sum
 };

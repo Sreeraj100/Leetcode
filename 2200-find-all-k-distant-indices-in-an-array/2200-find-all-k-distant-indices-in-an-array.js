d6605b4b805a2635da115c8fc09cb7ec0641711b
Sreeraj.R <sreerajsrgmanu2@gmail.com>
2026-06-15 00:42:29 +0530

    Time: 7 ms (50.63%), Space: 58 MB (59.49%) - LeetHub

---
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} key
+ * @param {number} k
+ * @return {number[]}
+ */
+var findKDistantIndices = function(nums, key, k) {
+    let index = []
+    let n = nums.length
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (nums[j] === key && Math.abs(i - j) <= k) {
+                index.push(i);
+                break;
+            }
+        }
+    }
+    return index
+}
